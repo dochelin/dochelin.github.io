307624e4180d9f268333c7392c85b57ed9d71a9f
gplee@pixelhub.co.kr <gplee@pixelhub.co.kr>
2020-06-05 10:11:48 +0900

    20200606 map edition commit

---
+++ js/domMgr.js
@@ -0,0 +1,11 @@
+
+
+
+function getItems (itemType, itemObject){
+	var classDefault = 'row justify-content-center no-gutters';
+	
+	var anchor = $('<div/>', {
+		className: 'classDefault',
+		text: '',
+	});
+}
---
+++ js/vue/nav.js
@@ -0,0 +1,16 @@
+var navRender = new Vue({
+  el : '#sample',
+  data : {
+    items : [
+      {data : 'about'},
+	  {data : 'One<br>Dorongthy'},
+	  {data : 'Two<br>Dorongthy'},
+	  {data : 'Three<br>Dorongthy'},
+	  {data : 'Contact'}
+	]
+  }
+
+reder : function()
+
+
+});
